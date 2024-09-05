40dc1b540b57da137281c1b85bfef79c6e9e3c64
костякрут23 <kostyua7@gmail.com>
2024-09-05 11:49:21 +0300

    Some files update

---
+++ assets/js/scripts.js
@@ -0,0 +1,54 @@
+const localization = {
+    'ru': {
+        title: "Сайт находится на очередной перестройке :(",
+        message: "Зайдите как-нибудь позже.",
+        notification: "📋 Username скопирован!"
+    },
+    'en': {
+        title: "The website is under reconstruction :(",
+        message: "Please visit again later.",
+        notification: "📋 Username copied!"
+    },
+    'uk': {
+        title: "Сайт знову знаходиться на перебудові :(",
+        message: "Зайдіть якось пізніше.",
+        notification: "📋 Username скопійован!"
+    }
+};
+
+const userLang = navigator.language || navigator.userLanguage;
+
+function setLocalizedText(language) {
+    const text = localization[language] || localization['ru'];
+    document.querySelector('h1').textContent = text.title;
+    document.querySelector('p').textContent = text.message;
+}
+
+if (userLang.startsWith('uk')) {
+    setLocalizedText('uk');
+} else if (userLang.startsWith('en')) {
+    setLocalizedText('en');
+} else {
+    setLocalizedText('ru');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const discordLogo = document.getElementById("discord-logo");
+    const notification = document.getElementById("discord-notification");
+
+    const language = userLang.startsWith('uk') ? 'uk' : userLang.startsWith('en') ? 'en' : 'ru';
+    const notificationMessage = localization[language].notification;
+
+    discordLogo.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        notification.textContent = notificationMessage;
+        notification.classList.add("show");
+
+        navigator.clipboard.writeText("mr.rais");
+
+        setTimeout(function () {
+            notification.classList.remove("show");
+        }, 3000);
+    });
+});
